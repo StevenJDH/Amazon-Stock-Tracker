7c1ceaa011dde9f7b42e47719ef2453a0cbcc881
Steven Jenkins De Haro <20492442+StevenJDH@users.noreply.github.com>
2022-06-12 12:49:12 +0200

    Updated entry point as per newer project templates

--- Amazon Stock Tracker/Program.cs
+++ Amazon Stock Tracker/Program.cs
@@ -16,37 +16,29 @@
  * along with Amazon Stock Tracker.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+namespace Amazon_Stock_Tracker;
 
-namespace Amazon_Stock_Tracker
+static class Program
 {
-    static class Program
-    {
-        static readonly Mutex mutex = new Mutex(initiallyOwned: true, name: "87f6c812-3328-44bd-92c6-b64df5e6d601");
+    static readonly Mutex mutex = new(initiallyOwned: true, name: "87f6c812-3328-44bd-92c6-b64df5e6d601");
 
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        if (mutex.WaitOne(timeout: TimeSpan.Zero, exitContext: true))
         {
-            if (mutex.WaitOne(timeout: TimeSpan.Zero, exitContext: true))
-            {
-                Application.SetHighDpiMode(HighDpiMode.SystemAware);
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new FrmMain());
-            }
-            else
-            {
-                MessageBox.Show($"You can only run one instance of {Application.ProductName} at a time.",
-                    Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            }
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new FrmMain());
+        }
+        else
+        {
+            MessageBox.Show($"You can only run one instance of {Application.ProductName} at a time.",
+                Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
         }
     }
 }
